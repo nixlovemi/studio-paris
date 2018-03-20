9703713e091faa047d759d907d323d6bf0eeb0ee
nixlovemi <nixlovemi@gmail.com>
2018-03-20 12:00:43 -0300

    Tooltip nos menus.

--- StdParis/HTML/master.js
+++ StdParis/HTML/master.js
@@ -3,4 +3,18 @@ $( document ).ready(function() {
         adaptiveHeight: true,
         captions: false
     });
+    $('.tooltip').tooltipster({
+        contentCloning: true,
+        contentAsHTML: true,
+        interactive: true,
+        side: 'bottom',
+        theme: 'tooltipster-light'
+    });
+    $('.tooltip-footer').tooltipster({
+        contentCloning: true,
+        contentAsHTML: true,
+        interactive: true,
+        side: 'top',
+        theme: 'tooltipster-light'
+    });
 });
