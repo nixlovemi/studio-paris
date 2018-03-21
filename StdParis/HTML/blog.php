9270f631300a633c5651c9840335e487c683de49
nixlovemi <nixlovemi@gmail.com>
2018-03-21 08:56:19 -0300

    Página home do blog.

---
+++ StdParis/HTML/blog.php
@@ -0,0 +1,191 @@
+<?php
+include("header.php");
+?>
+
+<div id="pre-content">
+    <div class="content-wrap">
+        <h1 class="blog-section-title">BLOG</h1>
+    </div>
+</div>
+
+<div id="content" class="mt-40">
+    <div class="content-wrap">
+        <div class="section group">
+          <div class="col span_3_of_3">
+            <ul id="blog-tag-list">
+              <li>
+                <a href="javascript:;" class="button">PROJETO</a>
+              </li>
+              <li>
+                <a href="javascript:;" class="button">INSPIRAÇÃO</a>
+              </li>
+              <li>
+                <a href="javascript:;" class="button">NOTÍCIAS</a>
+              </li>
+              <li>
+                <a href="javascript:;" class="button">PAREDE DECORADA</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+
+        <div class="section group blog-home">
+            <div class="col span_1_of_3 bh-item">
+                <div class="image mb-20">
+                    <img alt="" src="http://ilivrevida.livrevida.com.br/2008/07/Decoracao-casa-pequena-3.jpg" />
+                </div>
+                <div class="metadata mb-20">
+                    01/03/2018
+                </div>
+                <div class="title mb-20">
+                    <h3>PROJETO: Instalação casa Jundiaí</h3>
+                </div>
+                <div class="content mb-10">
+                    <p>Spicy jalapeno bacon ipsum dolor amet swine fugiat pork belly, est landjaeger magna sunt tri-tip chicken pork chop picanha aute. Anim porchetta spare ribs ut, minim jerky meatball doner voluptate in capicola fatback swine. Burgdoggen ground round labore, commodo tri-tip t-bone consequat capicola. Esse dolore et, veniam sed sausage proident strip steak ham hock labore ham elit cow.</p>
+                </div>
+
+                <p class="bh-tags mb-15">
+                  <a href="/blogs/blog/tagged/ledgestones">
+                    <i class="fas fa-tag"></i> projeto
+                  </a>
+                  <a href="/blogs/blog/tagged/ledgestones">
+                    <i class="fas fa-tag"></i> parede decorada
+                  </a>
+                </p>
+                <p>
+                    <a href="javascript:;" class="button">LEIA MAIS</a>
+                </p>
+            </div>
+            <div class="col span_1_of_3 bh-item">
+                <div class="image mb-20">
+                    <img alt="" src="https://i.pinimg.com/originals/57/7d/c5/577dc5a3a4c44f558de858c4ee6ff581.jpg" />
+                </div>
+                <div class="metadata mb-20">
+                    01/03/2018
+                </div>
+                <div class="title mb-20">
+                    <h3>PROJETO: Instalação casa Americana</h3>
+                </div>
+                <div class="content mb-20">
+                    <p>Spicy jalapeno bacon ipsum dolor amet swine fugiat pork belly, est landjaeger magna sunt tri-tip chicken pork chop picanha aute. Anim porchetta spare ribs ut, minim jerky meatball doner voluptate in capicola fatback swine. Burgdoggen ground round labore, commodo tri-tip t-bone consequat capicola. Esse dolore et, veniam sed sausage proident strip steak ham hock labore ham elit cow.</p>
+                </div>
+
+                <p class="bh-tags mb-15">
+                  <a href="/blogs/blog/tagged/ledgestones">
+                    <i class="fas fa-tag"></i> projeto
+                  </a>
+                  <a href="/blogs/blog/tagged/ledgestones">
+                    <i class="fas fa-tag"></i> inspiração
+                  </a>
+                </p>
+                <p>
+                    <a href="javascript:;" class="button">LEIA MAIS</a>
+                </p>
+            </div>
+            <div class="col span_1_of_3 bh-item">
+                <div class="image mb-20">
+                    <img alt="" src="https://media.agenteimovel.com.br/images/noticias/2012/08/A-luminosidade-natural-%C3%A9-uma-fonte-de-energia-para-a-renova%C3%A7%C3%A3o-do-ambiente-Via-Original-e-Gratis.jpg" />
+                </div>
+                <div class="metadata mb-20">
+                    01/03/2018
+                </div>
+                <div class="title mb-20">
+                    <h3>PROJETO: Instalação casa SBO</h3>
+                </div>
+                <div class="content mb-20">
+                    <p>Spicy jalapeno bacon ipsum dolor amet swine fugiat pork belly, est landjaeger magna sunt tri-tip chicken pork chop picanha aute. Anim porchetta spare ribs ut, minim jerky meatball doner voluptate in capicola fatback swine. Burgdoggen ground round labore, commodo tri-tip t-bone consequat capicola. Esse dolore et, veniam sed sausage proident strip steak ham hock labore ham elit cow.</p>
+                </div>
+
+                <p class="bh-tags mb-15">
+                  <a href="/blogs/blog/tagged/ledgestones">
+                    <i class="fas fa-tag"></i> notícia
+                  </a>
+                </p>
+                <p>
+                    <a href="javascript:;" class="button">LEIA MAIS</a>
+                </p>
+            </div>
+        </div>
+
+        <div class="section group blog-home">
+            <div class="col span_1_of_3 bh-item">
+                <div class="image mb-20">
+                    <img alt="" src="http://ilivrevida.livrevida.com.br/2008/07/Decoracao-casa-pequena-3.jpg" />
+                </div>
+                <div class="metadata mb-20">
+                    01/03/2018
+                </div>
+                <div class="title mb-20">
+                    <h3>PROJETO: Instalação casa Jundiaí</h3>
+                </div>
+                <div class="content mb-10">
+                    <p>Spicy jalapeno bacon ipsum dolor amet swine fugiat pork belly, est landjaeger magna sunt tri-tip chicken pork chop picanha aute. Anim porchetta spare ribs ut, minim jerky meatball doner voluptate in capicola fatback swine. Burgdoggen ground round labore, commodo tri-tip t-bone consequat capicola. Esse dolore et, veniam sed sausage proident strip steak ham hock labore ham elit cow.</p>
+                </div>
+
+                <p class="bh-tags mb-15">
+                  <a href="/blogs/blog/tagged/ledgestones">
+                    <i class="fas fa-tag"></i> projeto
+                  </a>
+                  <a href="/blogs/blog/tagged/ledgestones">
+                    <i class="fas fa-tag"></i> parede decorada
+                  </a>
+                </p>
+                <p>
+                    <a href="javascript:;" class="button">LEIA MAIS</a>
+                </p>
+            </div>
+            <div class="col span_1_of_3 bh-item">
+                <div class="image mb-20">
+                    <img alt="" src="https://i.pinimg.com/originals/57/7d/c5/577dc5a3a4c44f558de858c4ee6ff581.jpg" />
+                </div>
+                <div class="metadata mb-20">
+                    01/03/2018
+                </div>
+                <div class="title mb-20">
+                    <h3>PROJETO: Instalação casa Americana</h3>
+                </div>
+                <div class="content mb-20">
+                    <p>Spicy jalapeno bacon ipsum dolor amet swine fugiat pork belly, est landjaeger magna sunt tri-tip chicken pork chop picanha aute. Anim porchetta spare ribs ut, minim jerky meatball doner voluptate in capicola fatback swine. Burgdoggen ground round labore, commodo tri-tip t-bone consequat capicola. Esse dolore et, veniam sed sausage proident strip steak ham hock labore ham elit cow.</p>
+                </div>
+
+                <p class="bh-tags mb-15">
+                  <a href="/blogs/blog/tagged/ledgestones">
+                    <i class="fas fa-tag"></i> projeto
+                  </a>
+                  <a href="/blogs/blog/tagged/ledgestones">
+                    <i class="fas fa-tag"></i> inspiração
+                  </a>
+                </p>
+                <p>
+                    <a href="javascript:;" class="button">LEIA MAIS</a>
+                </p>
+            </div>
+            <div class="col span_1_of_3 bh-item">
+                <div class="image mb-20">
+                    <img alt="" src="https://media.agenteimovel.com.br/images/noticias/2012/08/A-luminosidade-natural-%C3%A9-uma-fonte-de-energia-para-a-renova%C3%A7%C3%A3o-do-ambiente-Via-Original-e-Gratis.jpg" />
+                </div>
+                <div class="metadata mb-20">
+                    01/03/2018
+                </div>
+                <div class="title mb-20">
+                    <h3>PROJETO: Instalação casa SBO</h3>
+                </div>
+                <div class="content mb-20">
+                    <p>Spicy jalapeno bacon ipsum dolor amet swine fugiat pork belly, est landjaeger magna sunt tri-tip chicken pork chop picanha aute. Anim porchetta spare ribs ut, minim jerky meatball doner voluptate in capicola fatback swine. Burgdoggen ground round labore, commodo tri-tip t-bone consequat capicola. Esse dolore et, veniam sed sausage proident strip steak ham hock labore ham elit cow.</p>
+                </div>
+
+                <p class="bh-tags mb-15">
+                  <a href="/blogs/blog/tagged/ledgestones">
+                    <i class="fas fa-tag"></i> notícia
+                  </a>
+                </p>
+                <p>
+                    <a href="javascript:;" class="button">LEIA MAIS</a>
+                </p>
+            </div>
+        </div>
+    </div>
+</div>
+
+<?php
+include("footer.php");
---
+++ StdParis/HTML/header.php
@@ -0,0 +1,58 @@
+<!-- https://theme336-tile-stone.myshopify.com/ -->
+<!doctype html>
+<html lang="pt-br">
+    <head>
+        <meta charset="utf-8" />
+        <title>Studio Paris</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
+        <!--[if IE]>
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+        <![endif]-->
+
+        <link type="text/css" rel="stylesheet" href="css/fontawesome-free-5.0.8/web-fonts-with-css/css/fontawesome-all.min.css" />
+        <link type="text/css" rel="stylesheet" href="reset.css" />
+        <link type="text/css" rel="stylesheet" href="responsive-grid.css" />
+        <link type="text/css" rel="stylesheet" href="jquery/slippry-1.4.0/dist/slippry.css" />
+        <link type="text/css" rel="stylesheet" href="jquery/tooltipster/dist/css/tooltipster.bundle.min.css" />
+        <link type="text/css" rel="stylesheet" href="style.css" />
+    </head>
+    <body>
+        <div id="slider-top">
+            <div id="logo-top">
+                STUDIO PARIS
+            </div>
+            <div id="overlay-top"></div>
+            <div id="menu-top">
+                <ul>
+                    <li><a class="selected" href="javascript:;">HOME</a></li>
+                    <li><a href="./blog.php">BLOG</a></li>
+                    <li>
+                        <a class="tooltip" data-tooltip-content="#tooltip_menu_prod" href="javascript:;">PRODUTOS</a>
+                        <div class="tooltip_templates">
+                            <span id="tooltip_menu_prod">
+                                <ul>
+                                    <li>
+                                        <a href="javascript:;">Boiserie</a>
+                                    </li>
+                                </ul>
+                            </span>
+                        </div>
+                    </li>
+                    <li><a href="javascript:;">CONTATO</a></li>
+                </ul>
+            </div>
+
+
+            <ul id="out-of-the-box-demo">
+                <li>
+                    <a href="#slide1">
+                        <img alt="" src="http://tucasaderevista.es/wp-content/uploads/2013/02/Boiserie-Cl%C3%A1sicas1.jpg" />
+                    </a>
+                </li>
+                <li>
+                    <a href="#slide2">
+                        <img alt="" src="http://hilgil.com/i/2017/03/quarto-de-bebe-marrom-e-bege.jpg" />
+                    </a>
+                </li>
+            </ul>
+        </div>
